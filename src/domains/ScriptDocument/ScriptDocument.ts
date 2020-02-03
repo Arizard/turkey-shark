72d3df968f34f71d5066e7ccd397e2831c3c7b9c
Arie <aoldman@deputy.com>
2020-02-03 17:36:27 +1100

    add scriptdocument entity

---
+++ src/domains/ScriptDocument/ScriptDocument.ts
@@ -0,0 +1,157 @@
+export class ScriptDocument {
+    public id: string;
+    public scriptElements: Array<ScriptElement>;
+    public owner: string;
+    public name: string;
+
+    constructor(
+        id: string,
+        scriptElements: Array<ScriptElement>,
+        owner: string,
+        name: string,
+    ) {
+        this.id = id;
+        this.scriptElements = scriptElements;
+        this.owner = owner;
+        this.name = name;
+    }
+}
+
+enum ElementType {
+    H1,
+    H2,
+    Paragraph,
+    Outcome,
+}
+
+interface ScriptElement {
+    elementType: ElementType;
+    id: string;
+    owner: string;
+}
+
+export class H1ScriptElement implements ScriptElement {
+    public elementType: ElementType;
+    public id: string;
+    public owner: string;
+    public content: string;
+
+    constructor(
+        id: string,
+        owner: string,
+        content: string
+    ) {
+        this.elementType = ElementType.H1;
+        this.id = id;
+        this.owner = owner;
+        this.content = content;
+    }
+}
+
+export class H2ScriptElement implements ScriptElement {
+    public elementType: ElementType;
+    public id: string;
+    public owner: string;
+    public content: string;
+
+    constructor(
+        id: string,
+        owner: string,
+        content: string
+    ) {
+        this.elementType = ElementType.H2;
+        this.id = id;
+        this.owner = owner;
+        this.content = content;
+    }
+}
+
+export class ParagraphScriptElement implements ScriptElement {
+    public elementType: ElementType;
+    public id: string;
+    public owner: string;
+    public content: string;
+
+    constructor(id: string, owner: string, content: string) {
+        this.elementType = ElementType.Paragraph;
+        this.id = id;
+        this.owner = owner;
+        this.content = content;
+    }
+}
+
+export class OutcomeScriptElement implements ScriptElement {
+    public elementType: ElementType;
+    public id: string;
+    public owner: string;
+
+    public counts: string;
+    public moveName: string;
+    public outcome: string;
+    public cues: Array<ScriptCue>;
+
+    constructor(
+        id: string,
+        owner: string,
+        counts: string,
+        moveName: string,
+        outcome: string,
+        cues: Array<ScriptCue>,
+    ) {
+        this.elementType = ElementType.Outcome;
+        this.id = id;
+        this.owner = owner;
+        this.counts = counts;
+        this.moveName = moveName;
+        this.outcome = outcome;
+        this.cues = cues;
+    }
+
+}
+
+export class ScriptCue {
+    constructor(id: string, content: string, tag: ScriptTag) {
+        this.id = id;
+        this.content = content;
+        this.tag = tag;
+    }
+    public id: string;
+    public content: string;
+    public tag: ScriptTag;
+}
+
+export class ScriptTag {
+    public id: string;
+    public color: string;
+    public name: string;
+
+    constructor(id: string, color: string, name: string) {
+        this.id = id;
+        this.color = color;
+        this.name = name;
+    }
+}
+
+export interface ScriptDocumentRepository {
+    get: {(id: string): Promise<ScriptDocument>};
+    put: {(id: string, doc: ScriptDocument): void};
+    new: {(): Promise<string>}
+    find: {(...args: Array<any>): Promise<Array<ScriptDocument>>};
+}
+
+export class ScriptDocumentService {
+    private repo: ScriptDocumentRepository;
+    public owner: string;
+
+    constructor(repo: ScriptDocumentRepository, owner: string) {
+        this.repo = repo;
+        this.owner = owner;
+    }
+
+    async newDocument(name: string): Promise<ScriptDocument> {
+        let docId = await this.repo.new();
+        let doc = await this.repo.get(docId);
+        doc.name = name;
+        return doc
+    }
+}
